65313224b05d073420bf0565c6a3ae7e1a4a22e7
Patrick Wijntjes <patrick.wijntjes@gmail.com>
2013-11-06 11:14:23 +0100

    replace to other git

--- afstudeer_stage/orchid/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from models import Orchid
-
-class UploadPictureForm(forms.ModelForm):
-    
-    class Meta:
-        model = Orchid
-        fields = ('title', 'picture')
